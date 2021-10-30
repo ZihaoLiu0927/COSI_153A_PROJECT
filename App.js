c98273e121d76db60c132c0e9927d76ddba775f5
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2021-10-30 01:47:08 -0400

    delete irrelevant

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import MathResult from './components/MathQuiz'
 import 'react-native-gesture-handler';
 import NavPage from './components/NavPage'
 // import UserImagePicker from './components/ImagePicker.js'
@@ -8,7 +7,6 @@ import UserProfile from './components/UserProfile'
 import MyChart from './components/balanceChart'
 import StockListElement from './components/StockListElement'
 import SelectStock from './components/SelectStock'
-//import Quiz3 from './components/Quiz3.js'
 import GetDataFromPolygon from './components/GetDataFromPolygon'
 
 export default function App() {
